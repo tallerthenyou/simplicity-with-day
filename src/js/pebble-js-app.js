8eb1f71d6d4f4038df86cd65ebda9ad6cd3f305e
John Flanagan <john@jflan.com>
2013-11-08 00:08:49 -0600

    Formatting

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -82,13 +82,17 @@ var locationOptions = { "timeout": 15000, "maximumAge": 60000 };
 Pebble.addEventListener("ready",
                         function(e) {
                           console.log("connect!" + e.ready);
-                          window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+                          window.navigator.geolocation.getCurrentPosition(locationSuccess,
+                                                                          locationError,
+                                                                          locationOptions);
                           console.log(e.type);
                         });
 
 Pebble.addEventListener("appmessage",
                         function(e) {
-                          window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
+                          window.navigator.geolocation.getCurrentPosition(locationSuccess,
+                                                                          locationError,
+                                                                          locationOptions);
                           console.log(e.type);
                           console.log(e.payload.temperature);
                           console.log("message!");
